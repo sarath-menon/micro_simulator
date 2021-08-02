06849022213b41cb17617b4c6a281e2f4d3ce76c
smenon <smenon@ethz.ch>
2021-08-02 22:50:13 +0200

    added softbody

--- src/description/primitives/structural/softbody/softbody.cpp
+++ src/description/primitives/structural/softbody/softbody.cpp
@@ -1,13 +1,5 @@
 #include "softbody.h"
 
-#include "utils.h"
-#include <yaml-cpp/yaml.h>
-
-SoftBody::SoftBody(std::string parameter_path) {
-  YAML::Node yaml_file = YAML::LoadFile(parameter_path);
-
-  mass_ = yaml_file["mass"].as<float>(); // [kg]
-  inertia_matrix_ = yaml_to_eigen_matrix3d(yaml_file, "inertia_matrix");
-}
+// SoftBody::SoftBody(std::string parameter_path) {
 
 // Setter functions
--- src/description/primitives/structural/softbody/softbody.h
+++ src/description/primitives/structural/softbody/softbody.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <Eigen/Dense>
-#include <iostream>
-#include <yaml-cpp/yaml.h>
+#include <matrix/math.hpp>
 
 /// Represents a Soft body
 class SoftBody {
@@ -10,7 +8,7 @@ class SoftBody {
 private:
 public:
   /// Loads the soft body properties from the yaml file
-  SoftBody(std::string parameter_path);
+  // SoftBody(std::string parameter_path);
 
   // Variables
 protected:
@@ -20,24 +18,24 @@ protected:
   float mass_ = 0;
 
   /// Inertia matrix of the soft body
-  Eigen::Matrix3d inertia_matrix_;
+  matrix::SquareMatrix<float, 3> inertia_matrix_;
 
   // Position and Orietnation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Position of the soft body
-  Eigen::Vector3d position_;
+  matrix::Vector<float, 3> position_;
 
   /// Orientation of the soft body as Quaternion (q_0, q_x, q_y, q_z)
-  Eigen::Vector4d orientation_;
+  matrix::Quatf orientation_;
 
   // Time derivatives of Position and Orietnation
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   /// Time derivative of soft body position
-  Eigen::Vector3d position_dot_;
+  matrix::Vector<float, 3> position_dot_;
 
   /// Time derivative of quaternion representing soft body  orientation
-  Eigen::Vector4d orientation_dot_;
+  matrix::Vector<float, 4> orientation_dot_;
 
 public:
   /// Have to be defined in the child class
@@ -46,15 +44,15 @@ public:
   // Getter Functions
 public:
   /// Getter function
-  const float &mass() const { return mass_; }
-  /// Getter function
-  const Eigen::Matrix3d &inertia_matrix() const { return inertia_matrix_; }
-  /// Getter function
-  const Eigen::Vector3d &position() const { return position_; }
-  /// Getter function
-  const Eigen::Vector4d &orientation() const { return orientation_; }
-  /// Getter function
-  const Eigen::Vector3d &position_dot() const { return position_dot_; }
-  /// Getter function
-  const Eigen::Vector4d &orientation_dot() const { return orientation_dot_; }
+  // const float &mass() const { return mass_; }
+  // /// Getter function
+  // const Eigen::Matrix3d &inertia_matrix() const { return inertia_matrix_; }
+  // /// Getter function
+  // const Eigen::Vector3d &position() const { return position_; }
+  // /// Getter function
+  // const Eigen::Vector4d &orientation() const { return orientation_; }
+  // /// Getter function
+  // const Eigen::Vector3d &position_dot() const { return position_dot_; }
+  // /// Getter function
+  // const Eigen::Vector4d &orientation_dot() const { return orientation_dot_; }
 };
