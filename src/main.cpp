bb1b5d1bed4c0221c3116ee6902851235269e7bd
smenon <smenon@ethz.ch>
2021-08-03 20:15:30 +0200

    added units for most parameters

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,16 @@ int main() {
   QuadcopterFrame frame;
   BldcMotor motor[4];
 
-  float frame_mass = 0.3;
+  float frame_mass = 0.3; // kg
   float frame_intertia_matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
   float linear_drag_coeff = 1;
   float angular_drag_coeff = 0.025;
 
-  float motor_mass = 0.05;
+  float motor_mass = 0.05; // kg
   float motor_intertia_matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  int motor_max_speed = 20000;
-  int motor_min_speed = 1000;
-  float motor_distance = 0.05;
+  int motor_max_speed = 20000; // rpm
+  int motor_min_speed = 1000;  // rpm
+  float motor_distance = 0.05; /// m
 
   //////////////////////////////////////////////////////////
 
