353ecfa495cbb08bb0cf4c06eee7ed5a4b28830c
smenon <smenon@ethz.ch>
2021-08-03 19:17:36 +0200

    added well designed inertia matrix setter

--- src/description/primitives/structural/rigidbody/rigidbody.h
+++ src/description/primitives/structural/rigidbody/rigidbody.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "utils.h"
 #include <matrix/math.hpp>
 
 /// Represents a Rigid body
@@ -75,16 +76,21 @@ public:
   void set_mass(float mass) { mass_ = mass; }
 
   /// Getter function
-  void set_inertia_matrix(const float inertia_matrix[3][3]) {
-    inertia_matrix_(0, 0) = inertia_matrix[0][0];
-    inertia_matrix_(0, 1) = inertia_matrix[0][1];
-    inertia_matrix_(0, 2) = inertia_matrix[0][2];
-    inertia_matrix_(1, 0) = inertia_matrix[1][0];
-    inertia_matrix_(1, 1) = inertia_matrix[1][1];
-    inertia_matrix_(1, 2) = inertia_matrix[1][2];
-    inertia_matrix_(2, 0) = inertia_matrix[2][0];
-    inertia_matrix_(2, 1) = inertia_matrix[2][1];
-    inertia_matrix_(2, 2) = inertia_matrix[2][2];
+  // void set_inertia_matrix(const float inertia_matrix[3][3]) {
+  //   inertia_matrix_(0, 0) = inertia_matrix[0][0];
+  //   inertia_matrix_(0, 1) = inertia_matrix[0][1];
+  //   inertia_matrix_(0, 2) = inertia_matrix[0][2];
+  //   inertia_matrix_(1, 0) = inertia_matrix[1][0];
+  //   inertia_matrix_(1, 1) = inertia_matrix[1][1];
+  //   inertia_matrix_(1, 2) = inertia_matrix[1][2];
+  //   inertia_matrix_(2, 0) = inertia_matrix[2][0];
+  //   inertia_matrix_(2, 1) = inertia_matrix[2][1];
+  //   inertia_matrix_(2, 2) = inertia_matrix[2][2];
+  // }
+
+  void set_inertia_matrix(float temp[3][3]) {
+    matrix::SquareMatrix<float, 3> inertia_matrix(temp);
+    inertia_matrix_ = inertia_matrix;
   }
   /// Getter function
   void set_position(matrix::Vector<float, 3> position) { position_ = position; }
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ int main() {
 
   QuadcopterFrame frame;
   frame.set_mass(0.7);
-  float temp[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  frame.set_inertia_matrix(temp);
+  float intertia_matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  frame.set_inertia_matrix(intertia_matrix);
 
   /// Quadcopter motors (4)
   BldcMotor motor[4];
