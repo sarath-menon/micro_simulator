570d49630fcd48f586ba45f3b433f4e00d89b8f3
smenon <smenon@ethz.ch>
2021-09-08 11:18:55 +0200

    altitude controller working

--- quadcopter_sim/controller/pid_cascaded/pid_cascaded.cpp
+++ quadcopter_sim/controller/pid_cascaded/pid_cascaded.cpp
@@ -11,14 +11,11 @@ float PidCascadedController::altitude_controller(const Quadcopter &quad,
   float thrust_command =
       altitude_pid(altitude_error, k_p__z, k_i__z, k_d__z, dt);
 
-  std::cout << "raw thrust command:" << thrust_command << '\n';
-
   // Quadcopter Motors have a maximum and minimum speed limit
   thrust_command =
       limit(ff_thrust + thrust_command, quad.thrust_max(), quad.thrust_min());
 
-  std::cout << "Altitue error:" << altitude_error << '\n';
-  std::cout << "k_p__z:" << k_p__z << '\n';
+  // std::cout << "Altitude error:" << altitude_error << '\n';
 
   return thrust_command;
 };
